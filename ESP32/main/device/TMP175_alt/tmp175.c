88c64113025c89edfca1d3d2c27af97ca3ebf2ad
Amaury Graillat <amaury@amoweb.fr>
2021-11-13 00:38:18 +0100

    Configure precision.

--- ESP32/main/device/TMP175_alt/tmp175.c
+++ ESP32/main/device/TMP175_alt/tmp175.c
@@ -1,4 +1,5 @@
 #include "driver/i2c.h"
+#include "freertos/portmacro.h"
 
 static const i2c_port_t i2c_port = 0;
 
@@ -13,7 +14,7 @@ void tmp175_alt_init()
         .scl_io_num = 22, // IO22
         .sda_pullup_en = false,
         .scl_pullup_en = false,
-        .master.clk_speed = 2000
+        .master.clk_speed = 500
     };
 
     esp_err_t err = i2c_param_config(i2c_port /* I2C driver num 0 */, &i2cConfig);
@@ -24,39 +25,59 @@ void tmp175_alt_init()
 
     // Install driver
     i2c_driver_install(i2c_port, I2C_MODE_MASTER, 0, 0, ESP_INTR_FLAG_LEVEL1);
+}
 
-    ////////////// SELECT TEMPERATURE REGISTER //////////////////
+/*
+   Returns temp in Celsius degrees from TMP175
+   Note : resolution 1 Celsius degree
+ */
+double tmp175_alt_get_temp() {
+
+    esp_err_t ret;
+
+    printf("top\n");
+
+    // i2c_cmd_handle_t cmd_handle;
+#if 1
+    ////////////// UPDATE CONFIGURATION //////////////////
     i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
     i2c_master_start(cmd_handle);
     // Slave address
-    i2c_master_write_byte(cmd_handle, /* TMP75 address */ (0x49<<1) | 0, /* ack_en */ 1);
-    // Pointer register: Temperature register (P1 = P2 = 0)
-    i2c_master_write_byte(cmd_handle, 0, /* ack_en */ 0);
+    i2c_master_write_byte(cmd_handle, /* TMP75 address */ (0x49<<1) | 0 /* write */, /* ack_en */ 1);
+    // Pointer register: Configuration register (P1 = 0, P2 = 1)
+    i2c_master_write_byte(cmd_handle, 1, /* ack_en */ 1);
+    // Data byte 1
+    i2c_master_write_byte(cmd_handle, 0x60 | 0x01 | 0x80 /* 0110 0000 => 12 bits */, /* ack_en */ 1);
+    // Data byte 2
+    // i2c_master_write_byte(cmd_handle, 0, /* ack_en */ 1);
+
     i2c_master_stop(cmd_handle);
-    ret = i2c_master_cmd_begin(i2c_port, cmd_handle, 1000);
+    ret = i2c_master_cmd_begin(i2c_port, cmd_handle, 200);
     if(ret == ESP_FAIL) {
         printf("I2C #1 Error ACK not received.\n");
     } else if(ret != ESP_OK) {
         printf("I2C #1 Error 0x%x\n", ret);
     }
     i2c_cmd_link_delete(cmd_handle);
-}
+#endif
 
-/*
-   Returns temp in Celsius degrees from TMP175
-Note : resolution 1 Celsius degree
- */
-double tmp175_alt_get_temp() {
+    printf("tic\n");
+    vTaskDelay(portTICK_PERIOD_MS * 10);
+    printf("toc\n");
 
-    esp_err_t ret;
+    ////////////////////// READ TEMPERATURE /////////////////////
     uint8_t data[2] = {0, 0};
 
-    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
-    i2c_master_start(cmd_handle);
-    i2c_master_write_byte(cmd_handle, /* TMP75 address */ (0x49<<1) | 1, /* ack_en */ 1);
+    cmd_handle = i2c_cmd_link_create();
+    i2c_master_start(cmd_handle); 
+    // Slave address
+    i2c_master_write_byte(cmd_handle, /* TMP75 address */ (0x49<<1) | 0 /*write*/, /* ack_en */ 1);
+    // Pointer register: Temperature register (P1 = P2 = 0)
+    i2c_master_write_byte(cmd_handle, 0, /* ack_en */ 1);
+    i2c_master_write_byte(cmd_handle, /* TMP75 address */ (0x49<<1) | 1 /*read*/, /* ack_en */ 1);
     i2c_master_read(cmd_handle, data, /* data_len */ 2, /* ack_en */ 1);
     i2c_master_stop(cmd_handle);
-    ret = i2c_master_cmd_begin(i2c_port, cmd_handle, 1000);
+    ret = i2c_master_cmd_begin(i2c_port, cmd_handle, 200);
     if(ret == ESP_FAIL) {
         printf("I2C #2 Error ACK not received.\n");
     } else if(ret != ESP_OK) {
